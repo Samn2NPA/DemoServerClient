55577a971a11e58b6d176098a52a1de919d3f447
N.N.P.A <2npa2812@gmail.com>
2020-10-22 18:23:02 +0700

    [update Server1]

--- src/main/java/Server1.java
+++ src/main/java/Server1.java
@@ -1,6 +1,33 @@
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import sun.net.httpserver.DefaultHttpServerProvider;
+
 /**
  * Created by Phanh on 10/22/20.
  */
 public class Server1 {
+
+    private static final Vertx vertx = Vertx.vertx();
+
+    public static void main(String[] args) {
+        startServer1();
+    }
+
+    public static void startServer1() {
+        HttpServer server = vertx.createHttpServer();
+        server.requestHandler(request -> {
+            System.out.println("request: " + request.toString());
+
+            // This handler gets called for each request that arrives on the server
+            HttpServerResponse response = request.response();
+            response.putHeader("content-type", "text/plain");
+
+            // Write to the response and end it
+            response.end("Hello World!");
+        });
+
+        server.listen(8080);
+    }
 
 }
---
+++ src/main/java/Verticle.java
@@ -0,0 +1,17 @@
+import io.vertx.core.*;
+
+/**
+ * Created by Phanh on 10/22/20.
+ */
+public class Verticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+
+    }
+
+    @Override
+    public void stop(Promise<Void> stopPromise) throws Exception {
+
+    }
+}
